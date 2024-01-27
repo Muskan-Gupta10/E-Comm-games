b02c72554700e3a7d94e1fe7142a9cb3c15805eb
Muskan-Gupta10 <muskangupta1018@gmail.com>
2024-01-28 01:18:47 +0530

    image issue fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ export default function  Card( {element}){
     //console.log(element.attributes.Image.data.attributes.url)
     const[toggle,setToggle]=useState(true)
     const {addToCart}=useContext(cartContext)
-    const url = `https://e-comm-games-strapi.onrender.com/${element.attributes.Image.data.attributes.url}`
+    const url = `${element.attributes.Image.data.attributes.url}`
     console.log(url)
     const [quantity,setQuantity]=useState(1)
     //http://localhost:1337/uploads/FORZA_HORIZON_4e2c3bbab3.png
